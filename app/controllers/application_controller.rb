b78920657c84de677d8aac424bcaa2811c3c0463
Raveen K Sharma <raveen08@gmail.com>
2020-10-07 18:41:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,7 @@ class ApplicationController < Sinatra::Base
 		#your code here!
 		user = User.find_by(:username => params[:username])
     if user
+      
     redirect "/success"
     else
     redirect "/failure"
